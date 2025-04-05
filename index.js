1f35fbbadc8609a69366e7edd21b41429761be06
Azizullah khan <aziz.bin.aman16@gmail.com>
2025-04-05 11:53:06 +0000

    added live

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'));
 
 // Route to handle the default page
 app.get('/', function(request, response) {
-  response.send('Hello World this is from production branch!');
+  response.send('Hello World this is from live branch!');
 });
 
 // Listen on all interfaces (0.0.0.0), not just localhost
